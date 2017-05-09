2636d7ed46e1a7b189505837bb9301517523ad3f
Justin Geeslin <justin@geesl.in>
2017-05-09 18:05:19 -0500

    Adding support for custom setters and getters

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 function extend(a, b) {
   a._super = b
-  for(var key in b)
-      if(b.hasOwnProperty(key))
-          a[key] = b[key];
+  for(var key in b) {
+    if(b.hasOwnProperty(key)) {
+      a[key] = b[key];
+    }
+    // Does the property have a custom getter or setter?
+    if (typeof b.__lookupGetter__(key) == "function") {
+      console.log('found a getter for ' + key);
+      a.__defineGetter__(key, b.__lookupGetter__(key))
+    }
+    if (typeof b.__lookupSetter__(key) == "function") {
+      console.log('found a setter for ' + key);
+      a.__defineSetter__(key, b.__lookupSetter__(key))
+    }
+
+  }
+
   return a;
 }
 
