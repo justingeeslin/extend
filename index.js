a0e409bc1948a6fb37f1228c4804d4852efe72a0
Justin Geeslin <justin@geesl.in>
2017-06-09 15:42:52 -0500

    Removing logs

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function extend(a, b) {
     }
     // Does the property have a custom getter or setter?
     if (typeof b.__lookupGetter__(key) == "function") {
-      console.log('found a getter for ' + key);
+      // console.log('found a getter for ' + key);
       a.__defineGetter__(key, b.__lookupGetter__(key))
     }
     if (typeof b.__lookupSetter__(key) == "function") {
-      console.log('found a setter for ' + key);
+      // console.log('found a setter for ' + key);
       a.__defineSetter__(key, b.__lookupSetter__(key))
     }
 
